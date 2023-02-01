ade7e166839cd4224707b75694ca30c0882099b6
Mick Harrigan <mharrigan328@gmail.com>
2023-01-31 21:14:19 -0500

    Update the repo with some functions and updated CaptureDevice Component

--- src/components.rs
+++ src/components.rs
@@ -1,4 +1,12 @@
 use bevy::prelude::*;
+use opencv::videoio::*;
+use std::{
+    ptr,
+    sync::{Arc, Mutex},
+    thread::{self},
+};
 
 #[derive(Component)]
-pub struct CaptureDevice;
+pub struct CaptureDevice {
+    pub dev: Mutex<VideoCapture>,
+}
