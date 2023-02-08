1fd803129dfe9f4a095a760659fc9510d5239ada
Mick Harrigan <mharrigan328@gmail.com>
2023-02-08 14:53:15 -0500

    May have forgotten components.rs, whoops

--- src/components.rs
+++ src/components.rs
@@ -1,12 +1,1 @@
-use bevy::prelude::*;
-use opencv::videoio::*;
-use std::{
-    ptr,
-    sync::{Arc, Mutex},
-    thread::{self},
-};
-
-#[derive(Component)]
-pub struct CaptureDevice {
-    pub dev: Mutex<VideoCapture>,
-}
+// this is where the components of the imaging will live
