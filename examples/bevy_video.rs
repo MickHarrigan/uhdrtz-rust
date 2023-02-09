dc3ede20d435a29424b797c17194b398656690f2
Mick Harrigan <mharrigan328@gmail.com>
2023-02-08 20:38:53 -0500

    Rotating a still image works

--- examples/bevy_video.rs
+++ examples/bevy_video.rs
@@ -4,7 +4,24 @@ use nokhwa::pixel_format::RgbFormat;
 use nokhwa::utils::{
     CameraFormat, CameraIndex, FrameFormat, RequestedFormat, RequestedFormatType, Resolution,
 };
-use nokhwa::{Buffer, Camera};
+use nokhwa::Buffer;
+use nokhwa::Camera as PhysicalCamera;
+
+#[derive(Component)]
+struct VideoStream;
+
+impl VideoStream {
+    pub fn new(index: u32, format: RequestedFormat) -> Image {
+        // create a new camera connection a la the nokhwa example
+        let mut camera = PhysicalCamera::new(CameraIndex::Index(index), format)
+            .expect("Could not create a physical camera connection");
+
+        camera
+            .open_stream()
+            .expect("Could not open the camera stream");
+        todo!();
+    }
+}
 
 fn main() {
     App::new()
@@ -18,25 +35,38 @@ fn main() {
             ..default()
         }))
         .add_startup_system(setup)
+        .add_system(camera_rotation) // function that rotates the camera automatically, will update to be based on input next
         .run()
 }
 
-fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
+fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>, assets: Res<AssetServer>) {
     // next up is to open a camera (both physical camera for taking an image as well as the logical bevy one that looks at a plane)
     // then open a stream from the camera with the right settings
     // then constantly (read: every frame of the "game") get and image from the camera
     // and to display that image to a plane that a 2d camera is looking at
 
     // TODO: this should be filled in from the nokhwa stuff
-    let video_output: Handle<Image> = images.add(Image::default());
+    // let video_output: Handle<Image> = images.add(Image::default());
+    let video_output = assets.load("image.png");
 
     // TODO: this camera could be tweaked to change where and what it is looking at
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 
     commands.spawn(SpriteBundle {
         // the clone() could be redundant, so will have to check that in the coming time
         texture: video_output.clone(),
-        transform: Transform::from_xyz(0.0, 0.0, 0.0), // TODO: update the transform
+        transform: Transform::from_xyz(0.0, 0.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y), // TODO: update the transform
         ..default()
     });
+}
+
+fn camera_rotation(time: Res<Time>, mut query: Query<&mut Transform, With<Camera>>) {
+    // let sec = time.elapsed_seconds() * 0.2;
+    for mut transform in query.iter_mut() {
+        // *transform = Transform::from_xyz(sec.sin() * 5.0, 2.5, sec.cos() * 5.0)
+        transform.rotate_z(time.delta_seconds());
+    }
 }
