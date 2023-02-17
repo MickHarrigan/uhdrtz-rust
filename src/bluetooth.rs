a47ed2bf3697a364e9b23ba02c61357c2592d1db
Mick Harrigan <mharrigan328@gmail.com>
2023-02-17 14:20:53 -0500

    Updated the repo to have a new file in the backend

--- src/bluetooth.rs
+++ src/bluetooth.rs
@@ -1,11 +1,9 @@
-use bevy::{core_pipeline::clear_color, prelude::*};
+use bevy::prelude::*;
 use bevy_tokio_tasks::*;
 use futures::stream::StreamExt;
 use std::time::Duration;
 
-use btleplug::api::{
-    bleuuid::BleUuid, Central, CentralEvent, Manager as _, Peripheral as _, ScanFilter,
-};
+use btleplug::api::{Central, CentralEvent, Manager as _, Peripheral as _, ScanFilter};
 use btleplug::platform::{Adapter, Manager, Peripheral};
 use uuid::Uuid;
 
@@ -13,55 +11,15 @@ const PERIPHERAL_NAME_MATCH_FILTER: &str = "Arduino";
 
 const NOTIFY_CHARACTERISTIC_UUID: Uuid = Uuid::from_u128(0x13012F00_F8C3_4F4A_A8F4_15CD926DA146);
 
-static COLORS: [Color; 5] = [
-    Color::RED,
-    Color::GREEN,
-    Color::BLUE,
-    Color::WHITE,
-    Color::BLACK,
-];
-
 #[derive(Resource)]
 struct ZoetropeRotation(i8);
 
-// Notes:  Next up is to make this work with changing a resource that is user defined.
-// This resource will be the "rotation angle" that is desired for the image.
-// After setting this the next step is to access that Resource every frame and update the output based on that.
-
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(TokioTasksPlugin::default())
-        .insert_resource(ZoetropeRotation(0))
-        .add_system(bevy::window::close_on_esc)
-        .add_startup_system(demo)
-        .run();
-}
-
 fn demo(rt: ResMut<TokioTasksRuntime>, mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-    // rt.spawn_background_task(update_colors);
     rt.spawn_background_task(update_rotation);
 }
 
-async fn update_colors(mut ctx: TaskContext) {
-    let mut color_index = 0;
-    loop {
-        ctx.run_on_main_thread(move |ctx| {
-            if let Some(mut clear_color) = ctx.world.get_resource_mut::<ClearColor>() {
-                clear_color.0 = COLORS[color_index];
-                println!("Changed color to {:?}", clear_color.0);
-            }
-        })
-        .await;
-        color_index = (color_index + 1) % COLORS.len();
-        tokio::time::sleep(Duration::from_secs(2)).await;
-    }
-}
-
 async fn update_rotation(mut ctx: TaskContext) {
-    // this should update the Res<ArduinoOutput>
-
     // first part should be run only once, while the sending part should be done constantly
     let manager = Manager::new().await.unwrap();
     let adapter_list = manager.adapters().await.unwrap();
@@ -115,13 +73,9 @@ async fn update_rotation(mut ctx: TaskContext) {
                             println!("Checking characteristic {:?}", characteristic);
                             // Subscribe to notifications from the characteristic with the selected
                             // UUID.
-                            if characteristic.uuid == NOTIFY_CHARACTERISTIC_UUID
-                            // && characteristic.properties.contains(CharPropFlags::NOTIFY)
-                            {
+                            if characteristic.uuid == NOTIFY_CHARACTERISTIC_UUID {
                                 println!("Subscribing to characteristic {:?}", characteristic.uuid);
                                 peripheral.subscribe(&characteristic).await.unwrap();
-                                // Print the first 4 notifications received.
-                                // Process while the BLE connection is not broken or stopped.
                                 let mut notification_stream =
                                     peripheral.notifications().await.unwrap();
                                 loop {
@@ -131,6 +85,7 @@ async fn update_rotation(mut ctx: TaskContext) {
                                                 ctx.world.get_resource_mut::<ZoetropeRotation>()
                                             {
                                                 let val = *data.value.iter().next().unwrap_or(&0); // change this to fit the correct rotation values
+                                                #[allow(unused_assignments)]
                                                 let mut out: i8 = 0;
                                                 if val > 128 {
                                                     out = -1 * (255 - val) as i8;
