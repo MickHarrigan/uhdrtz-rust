4f484da2da8afd64e29b9b89b01d293e265589ae
Mick Harrigan <mharrigan328@gmail.com>
2023-02-28 12:17:04 -0500

    Update to include embedding the assets into the executable

--- examples/uhdrtz.rs
+++ examples/uhdrtz.rs
@@ -2,20 +2,26 @@
 // This will also be what the final binary should be made of and deployed from.
 use bevy::prelude::*;
 use bevy::window::PresentMode;
+use bevy_embedded_assets::EmbeddedAssetPlugin;
 use uhdrtz::prelude::*;
 
 fn main() {
     App::new()
         // this plugin stuff here could be set into a large Zoetrope plugin that is controlled in the library itself
         // thus that the actual example is just adding in that one plugin to the bevy system and its done
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            window: WindowDescriptor {
-                mode: WindowMode::BorderlessFullscreen,
-                present_mode: PresentMode::Fifo,
-                ..default()
-            },
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .build()
+                .add_before::<bevy::asset::AssetPlugin, _>(EmbeddedAssetPlugin)
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        mode: WindowMode::BorderlessFullscreen,
+                        present_mode: PresentMode::Fifo,
+                        ..default()
+                    },
+                    ..default()
+                }),
+        )
         .add_plugin(ZoetropePlugin)
         .run()
 }
