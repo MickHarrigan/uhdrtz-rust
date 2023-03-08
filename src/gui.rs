091322ac95986bfc868a5aa3ea2edae19a0b653b
Christian <christian.lostoski@gmail.com>
2023-03-08 18:44:05 -0500

    Added Color Picker option with Dummy variable

--- src/gui.rs
+++ src/gui.rs
@@ -47,11 +47,11 @@ pub fn gui_full(
     mut query: Query<&mut Transform, With<Camera>>,
     mut color_settings: ResMut<ColorSettings>,
 ) {
+    let mut color: egui::Color32 = Default::default();
     egui::Window::new("Effects")
         .vscroll(true)
         .open(&mut ui_state.is_window_open)
         .show(egui_ctx.ctx_mut(), |ui| {
-            ui.label("Color Section");
             ui.add(
                 egui::Slider::new(&mut color_settings.brightness, 0.0..=100.0)
                     .text("Brightness")
@@ -77,6 +77,9 @@ pub fn gui_full(
                     .text("White Balance")
                     .show_value(true),
             );
+            ui.label("Color Section");
+            ui.color_edit_button_srgba(&mut color);
+            ui.end_row();
         });
     egui::Window::new("Masks")
         .vscroll(true)
