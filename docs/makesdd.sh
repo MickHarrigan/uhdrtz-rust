6547a03de955808ee8c737f000661f34b85910a6
Daniel Cleaver <daniel.cleaver1@gmail.com>
2023-05-02 23:05:24 -0400

    Fix compatability with debian vm

--- docs/makesdd.sh
+++ docs/makesdd.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/sh
 
-pandoc sdd.md -o sdd.pdf -t html --template=templates/sdd.html5 --number-sections -M title="System Design Document" -V mainfont="Noto Serif" -V maxwidth=8.5in -V backgroundcolor=white -V margin-left=1in -V margin-right=1in -V margin-top=1in -V margin-bottom=1in -V papersize=Letter --verbose
+pandoc sdd.md -o sdd.pdf -t html --template=templates/sdd.html5 --number-sections -M title="System Design Document" -V mainfont="Noto Serif" -V maxwidth=8.5in -V backgroundcolor=white -V margin-left=1in -V margin-right=1in -V margin-top=1in -V margin-bottom=1in -V papersize=Letter --pdf-engine-opt=--enable-local-file-access
 
-firefox --new-window sdd.pdf
-
-#pandoc sdd.md -o sdd.html --template=templates/default.html5 --number-sections -M title="System Design Document" -V mainfont="Noto Serif" -V maxwidth=8.5in -V backgroundcolor=white -V margin-left=1in -V margin-right=1in -V margin-top=1in -V margin-bottom=1in -V papersize=Letter
-
-#firefox --new-window sdd.html
+#firefox --new-window sdd.pdf
