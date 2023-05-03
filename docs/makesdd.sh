41efc81d993d421c9871cb734119dd3b902f0bf8
Daniel Cleaver <daniel.cleaver1@gmail.com>
2023-05-02 22:58:09 -0400

    Work on templates for different pandoc versions

--- docs/makesdd.sh
+++ docs/makesdd.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-pandoc sdd.md -o sdd.pdf -t html --template=templates/default.html5 --number-sections -M title="System Design Document" -V mainfont="Noto Serif" -V maxwidth=8.5in -V backgroundcolor=white -V margin-left=1in -V margin-right=1in -V margin-top=1in -V margin-bottom=1in -V papersize=Letter --verbose
+pandoc sdd.md -o sdd.pdf -t html --template=templates/sdd.html5 --number-sections -M title="System Design Document" -V mainfont="Noto Serif" -V maxwidth=8.5in -V backgroundcolor=white -V margin-left=1in -V margin-right=1in -V margin-top=1in -V margin-bottom=1in -V papersize=Letter --verbose
 
 firefox --new-window sdd.pdf
 
